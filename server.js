cff5e0d916dde6739ff4e98ece2b399a6f927a24
ahmadbadawikr <ahmadbadawikr@outlook.com>
2020-09-27 02:17:53 +0700

    yay its working

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+
+//load env
+dotenv.config({ path: './config.env'});
+
+const app = express();
+
+// dev logging
+if(process.env.NODE_ENV==='development'){
+    app.use(morgan('dev'));
+}
+
+
+//profile routes
+app.use('/api/v1/profile', require('./routes/profile'));
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, ()=> {
+    console.log(`Server listening on port ${process.env.NODE_ENV} mode on port ${port}`);
+});
